865eed702154a221a953b405b2dfcea8adbedf03
Lalitha <lchitharanja@vmware.com>
2019-04-10 17:05:03 +0530

    Fix trigger_test_sqlserver test case failure - use SET NOCOUNT ON so that the number of rows affected by INSERT   statement is returned instead of the number of rows affected by the trigger - Refer https://techcommunity.microsoft.com/t5/SQL-Server/UPDATE-with-OUTPUT-clause-8211-Triggers-8211-and-SQLMoreResults/ba-p/383457

--- lib/active_record/connection_adapters/sqlserver/database_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/database_statements.rb
@@ -288,9 +288,11 @@ module ActiveRecord
                     end
 
                     <<~SQL.squish
+                      SET NOCOUNT ON
                       DECLARE @ssaIdInsertTable table (#{pk_and_types.map { |pk_and_type| "#{pk_and_type[:quoted]} #{pk_and_type[:id_sql_type]}"}.join(", ") });
                       #{sql.dup.insert sql.index(/ (DEFAULT )?VALUES/i), " OUTPUT #{ pk_and_types.map { |pk_and_type| "INSERTED.#{pk_and_type[:quoted]}" }.join(", ") } INTO @ssaIdInsertTable"}
                       SELECT #{pk_and_types.map {|pk_and_type| "CAST(#{pk_and_type[:quoted]} AS #{pk_and_type[:id_sql_type]}) #{pk_and_type[:quoted]}"}.join(", ")} FROM @ssaIdInsertTable
+                      SET NOCOUNT OFF
                     SQL
                   else
                     returning_columns = returning || Array(pk)
